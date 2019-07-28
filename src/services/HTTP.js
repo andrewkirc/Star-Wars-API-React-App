0ef7819ac62faf957e9e798f9c3f1fc0087147f8
Andrew Kirchofer <andrewkirchofer@gmail.com>
2019-07-28 10:42:17 -0400

    UPDATED: HTTP.test.js

--- src/services/HTTP.js
+++ src/services/HTTP.js
@@ -1,11 +1,11 @@
 export default class Request {
-  /** HTTP Request JSON
+  /** HTTP Request
    * @param {String} url - Accepts URL
    * @param {Number} timeout - Request timeout in milliseconds.
    * @param {Object} [options] - Accepts Fetch API options (https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#Supplying_request_options)
    * @returns {Object} { status: 200, data: {} }
    */
-  static async requestJSON(url, timeout, options) {
+  static async request(url, timeout, options) {
     try {
       const resp = await fetch(url, options);
       const jsonData = await resp.json();
--- src/services/__tests__/HTTP.test.js
+++ src/services/__tests__/HTTP.test.js
@@ -2,8 +2,12 @@ import http from '../HTTP';
 
 const url = "https://swapi.co/api/people/?format=json";
 describe('HTTP Class', () => {
-    it('Should retrieve all Star Wars characters.', async () => {
+    it('Should successfully make HTTP request.', async () => {
         const response = await http.request(url, 10000);
-        expect(response).toEqual(200);
+        expect(response.status).toEqual(200);
     });
-});
+    it('Should successfully parse JSON response.', async () => {
+        const response = await http.request(url, 10000);
+        expect(response.data.results[0].name).toEqual("Luke Skywalker");
+    });
+}, 15000);
